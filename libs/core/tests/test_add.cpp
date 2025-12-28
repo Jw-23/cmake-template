ceeb86284f11f543c29c82a9b1b43316407154c0
junwei <wjw9826@gmail.com>
2025-12-28 21:19:15 +0800

    change test_file

--- libs/core/tests/test_add.cpp
+++ libs/core/tests/test_add.cpp
@@ -1,7 +1,11 @@
 #include <corelib/simple.hpp>
 #include <gtest/gtest.h>
+#include <optional>
+
 
 TEST(MathTest, AddPositiveNumver)
 {
+    std::optional value(3);
+    
     EXPECT_EQ(corelib::add(3, 4), 7);
 }
